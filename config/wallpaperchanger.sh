950d6087931c0c0abac1aeb44c732210eb96b15e
Ales Lerch <ales.lerch@gmail.com>
2016-10-18 15:27:42 +0200

    install.sh added,config added

---
+++ config/wallpaperchanger.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+
+/usr/bin/osascript $HOME/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt
+
+exit 0
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+################################################################################
+# Shell script for installing Wallpaper-Changer on local machine.
+#
+# Script only works with OSX, while installing use "chmod u+x install.sh" and
+#"./install.sh", or type "sh install.sh"in your console/shell/Terminal.
+#
+# The commands are built to run once, e.g. to copy some directories and files,
+# but it's designed to end if some rules are broken on run.
+#
+################################################################################
+
+# Global variables
+LOCAL=/usr/local/bin
+TARGET=$HOME/Pictures/Wallpapers
+DIR="Time of Day"
+SCRIPT="wallpaperchanger.sh"
+PLIST="com.wallpaperchanger.plist"
+
+# Change directory to local Wallpapers if exist
+if [ ! -d $TARGET ]; then
+	echo "Directory $TARGET does not exist, creating";
+	mkdir -p "$TARGET";
+fi
+
+# Copying files to local dir
+echo "Creating local files"
+cp  -R "./$DIR" "$TARGET"
+cp "./wallpaper.scpt" "$TARGET"
+
+# Install bash script for changing wallpaper
+if [ -d $LOCAL ]; then
+	cp "./config/$SCRIPT" "$LOCAL/$SCRIPT";
+else
+	echo "Error $LOCAL not found ending installation";
+	exit 1;
+fi
+
+# Install check
+while true; do
+	if launchctl list | grep 'com.wallpaperchanger' -q; then
+		echo "Installation successfull";
+		break;
+	else
+		# Installing daemon plist
+		sudo cp "./config/$PLIST" "/Library/LaunchDaemons/";
+		sleep 1;
+		# Load plist
+		launchctl load -w /Library/LaunchDaemons/$PLIST;
+	fi
+done
+
