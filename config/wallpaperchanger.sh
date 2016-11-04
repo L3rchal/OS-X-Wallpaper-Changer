99a44171a9bed691cc23ae0a9f1965508c6b3600
Ales Lerch <ales.lerch@gmail.com>
2016-11-04 11:57:47 +0100

    wallpaper.sh fixed changing wallpaper

--- config/wallpaperchanger.sh
+++ config/wallpaperchanger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-
-/usr/bin/osascript $HOME/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt
+DIR=$(dscacheutil -q user | grep -A 3 -B 2 -e uid:\ 5'[0-9][0-9]' | awk -F'dir:' '{print $2}')
+/usr/bin/osascript $DIR/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt
 
 exit 0
