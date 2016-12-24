c4ada9609aab0e7a22b42197449124d5b33d6010
Ales Lerch <ales.lerch@gmail.com>
2016-12-24 14:53:51 +0100

    wallpaperchange.sh edit

--- config/wallpaperchanger.sh
+++ config/wallpaperchanger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-DIR=$(dscacheutil -q user | grep -A 3 -B 2 -e uid:\ 5'[0-9][0-9]' | awk -F'dir:' '{print $2}')
+DIR=$(dscacheutil -q user | grep -A 3 -B 2 -e uid:\ 5'[0-9][0-9]' | awk -F'dir:' '{print $2}' | ack 'Users')
 /usr/bin/osascript $DIR/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt
 
 exit 0
