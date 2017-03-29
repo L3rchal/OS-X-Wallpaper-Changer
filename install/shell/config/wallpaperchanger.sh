12ef4e3fdf69e6f88071a983926296f209f6d7df
Ales Lerch <ales.lerch@gmail.com>
2017-03-29 13:45:00 +0200

    fixing wallpaperchanger.sh

--- install/shell/config/wallpaperchanger.sh
+++ install/shell/config/wallpaperchanger.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 # ------------------------------------------------------------------
-# bug with users ... deprecated
-DIR=$(dscacheutil -q user | grep -A 3 -B 2 -e uid:\ 5'[0-9][0-9]' | awk -F'dir:' '{print $2}' | ack 'Users')
-/usr/bin/osascript $DIR/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt
+#DIR=$(dscacheutil -q user | grep -A 3 -B 2 -e uid:\ 5'[0-9][0-9]' | awk -F'dir:' '{print $2}' | ack 'Users')
 
-exit 0
+for user in $(who | awk '{print $1}' | uniq); do
+	#/usr/bin/osascript $user/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt ; \
+	if [ -e "/Users/$user/Desktop" ]; then
+		osascript /Users/$user/Pictures/Wallpapers/Time\ of\ Day/wallpaper.scpt ;
+		echo $user ;
+	fi
+done
